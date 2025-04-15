124544bafbf61c83845b9e33c6e0b096d9a11501
vinaneosoft <vinaneosoft@gmail.com>
2025-04-15 13:04:26 +0530

    commit

--- src/components/ReactEmployees.jsx
+++ src/components/ReactEmployees.jsx
@@ -1,9 +1,14 @@
 /* looping using javascript , JSX and expressions */
 function ReactEmployees(){
-    const reactEmps=['Suchita','Kiran','Karan']
+    const oltype="a";
+    const reactEmps=['Suchita','Kiran','Karan',"Pooja"] //string[]
+    const lielements=reactEmps.map(emp=><li>{emp}</li>); //React.JSX.Element[]
     return (
-        <p>{reactEmps}</p>
+       <ol type={oltype}> 
+           {lielements}
+       </ol>
     )
 }
 
 export default ReactEmployees
+/* type = 1, A, a, I, i */
