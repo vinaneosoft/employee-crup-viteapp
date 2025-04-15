d1b3b2246cce4f46922996dbdeaeb0fbb3581fa6
vinaneosoft <vinaneosoft@gmail.com>
2025-04-15 11:49:52 +0530

    commit

--- src/components/Learning.jsx
+++ src/components/Learning.jsx
@@ -4,15 +4,24 @@ import React from "react";
 
 function Learning(){
     let department="JS";
+    let married=true;
     return(
-      <React.Fragment>
-            <h5>Ternary Operator in JSX Expression</h5>
+      <>
+           <div>
+           <h5>Ternary Operator in JSX Expression (if then else statements)</h5>
             <p>
                 {department.toUpperCase()=='DN'
                 ?<b>you are allowed to attend training</b>
                 :<i>NOT ALLOWED</i>}
             </p>
-      </React.Fragment>
+           </div>
+           <div>
+            <h5>&& operator (if then statements)</h5>
+            <p>
+                {married && <mark>Employee is married</mark>}
+            </p>
+           </div>
+      </>
     );
 }
 
