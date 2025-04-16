795753ced5937641b85e1460eeb9829ee41d5726
vinaneosoft <vinaneosoft@gmail.com>
2025-04-16 13:30:07 +0530

    events

--- src/components/Learning.jsx
+++ src/components/Learning.jsx
@@ -13,12 +13,16 @@ function Learning(){
     textDecoration: "underline",
     color:'yellow'
     }
-
-   
     function handleClickEvent(){
-        alert("Button clicked.....");
+       alert("Button clicked.....");
     }
-    
+    function handleClickEvent2(event){
+        console.log(event);
+        alert(event.target)
+    }
+    function accessName(name){
+        alert("hello "+name);
+    }
 
     return(
       <>
@@ -52,6 +56,14 @@ function Learning(){
             <h5>Calling non parameterized function</h5>
             <p>
                 <button className="btn btn-primary" onClick={handleClickEvent}>CLICK</button>
+            </p>
+            <h5>Calling parameterized function (implicit parameter - event object)</h5>
+            <p>
+                <button className="btn btn-primary" onClick={handleClickEvent2}>CLICK 2</button>
+            </p>
+            <h5>Calling parameterized function (explicit parameter)</h5>
+            <p>
+                <button className="btn btn-primary" onClick={()=>accessName("veena")}>SEND NAME</button>
             </p>
            </div>
       </>
