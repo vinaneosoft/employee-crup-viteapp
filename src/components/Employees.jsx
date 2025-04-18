6361011792b85f6bd4646b4e083467e9215d4a71
vinaneosoft <vinaneosoft@gmail.com>
2025-04-18 11:58:25 +0530

    commit

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -7,12 +7,12 @@ function Employees(){
         {
             empId:234, empName:"veena patil", empDescription:"xyz", 
             empDepartment:"LD", empExperience:20, joiningDate:"12-Dec-2019", 
-            profilePic:'https://th.bing.com/th/id/OIP.yxFMf0fXHxmSFZv1rJ5ZFgHaLH?rs=1&pid=ImgDetMain' 
+            profilePic:'https://freedesignfile.com/upload/2018/11/Corporate-group-employees-Stock-Photo-05.jpg' 
         },
         {
             empId:111, empName:"veena patil", empDescription:"xyz", 
             empDepartment:"LD", empExperience:20, joiningDate:"12-Dec-2019", 
-            profilePic:'https://th.bing.com/th/id/OIP.yxFMf0fXHxmSFZv1rJ5ZFgHaLH?rs=1&pid=ImgDetMain' 
+            profilePic:'https://img.freepik.com/premium-photo/illustration-group-business-people-ai-generated_57312-8045.jpg' 
         },
         {
             empId:222, empName:"veena patil", empDescription:"xyz", 
@@ -22,7 +22,7 @@ function Employees(){
         {
             empId:333, empName:"veena patil", empDescription:"xyz", 
             empDepartment:"LD", empExperience:20, joiningDate:"12-Dec-2019", 
-            profilePic:'https://th.bing.com/th/id/OIP.yxFMf0fXHxmSFZv1rJ5ZFgHaLH?rs=1&pid=ImgDetMain' 
+            profilePic:'https://as2.ftcdn.net/v2/jpg/03/64/21/11/1000_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg' 
         }
     ]
 
