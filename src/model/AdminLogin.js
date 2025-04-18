4f5f2d19ddbe09e5473be52593295c25a67f8e4e
vinaneosoft <vinaneosoft@gmail.com>
2025-04-18 11:15:20 +0530

    comit

--- src/model/AdminLogin.js
+++ src/model/AdminLogin.js
@@ -1,7 +1,6 @@
 const username="neoadmin@neosoftmail.com";
 const password="neoadmin321";
 
-
 export function checkLogin(adminemail, adminpass){
     // connect to backend pass email, password
     //get : email, password return object
