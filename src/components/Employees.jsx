dfa7a624fb64b995ca299baa8a5fb23a7f3c2e23
vinaneosoft <vinaneosoft@gmail.com>
2025-04-18 11:56:03 +0530

    commit

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,8 +1,30 @@
 import EmployeeCard from "./EmployeeCard";
 
-
-
 function Employees(){
+     // manage many employees
+     // currently we create fake employees array
+    let employees=[
+        {
+            empId:234, empName:"veena patil", empDescription:"xyz", 
+            empDepartment:"LD", empExperience:20, joiningDate:"12-Dec-2019", 
+            profilePic:'https://th.bing.com/th/id/OIP.yxFMf0fXHxmSFZv1rJ5ZFgHaLH?rs=1&pid=ImgDetMain' 
+        },
+        {
+            empId:111, empName:"veena patil", empDescription:"xyz", 
+            empDepartment:"LD", empExperience:20, joiningDate:"12-Dec-2019", 
+            profilePic:'https://th.bing.com/th/id/OIP.yxFMf0fXHxmSFZv1rJ5ZFgHaLH?rs=1&pid=ImgDetMain' 
+        },
+        {
+            empId:222, empName:"veena patil", empDescription:"xyz", 
+            empDepartment:"LD", empExperience:20, joiningDate:"12-Dec-2019", 
+            profilePic:'https://th.bing.com/th/id/OIP.yxFMf0fXHxmSFZv1rJ5ZFgHaLH?rs=1&pid=ImgDetMain' 
+        },
+        {
+            empId:333, empName:"veena patil", empDescription:"xyz", 
+            empDepartment:"LD", empExperience:20, joiningDate:"12-Dec-2019", 
+            profilePic:'https://th.bing.com/th/id/OIP.yxFMf0fXHxmSFZv1rJ5ZFgHaLH?rs=1&pid=ImgDetMain' 
+        }
+    ]
 
     return (
         <EmployeeCard></EmployeeCard>
