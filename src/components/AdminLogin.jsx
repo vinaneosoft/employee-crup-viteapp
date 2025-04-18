505ecba791cce1d43d76c60c9a6bac6bb14186e7
vinaneosoft <vinaneosoft@gmail.com>
2025-04-18 11:05:52 +0530

    commit

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -1,21 +1,36 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { checkLogin } from "../model/AdminLogin";
 
 
 function AdminLogin(){
     let emailRef=useRef();
     let passwordRef=useRef();
-
+    const [loginStatus, setStatus]=useState("fail");
     function collectDetails(event){
         event.preventDefault();
         console.log(emailRef.current.value);
         console.log(passwordRef.current.value);
+
+        const flag=checkLogin(emailRef.current.value,passwordRef.current.value);
+        if(flag)
+            setStatus("success");
+        else
+            setStatus("fail")
+        /* if(flag)
+             navigate to dashboard
+        else
+             update the message on login form "incorrect username or password" */
     }
 
     return(
         <>
          <h4 className="text-center">ADMIN LOGIN FORM</h4>
+         <p>
+         <b>{loginStatus}</b>
+         </p>
          <div className="d-flex justify-content-center p-2">
             <form className="bg-secondary w-50 p-3" onSubmit={collectDetails}>
+           
                 <div className="mb-3">
                     <label htmlFor="emailRef" className="form-label">Email address</label>
                     <input type="email" ref={emailRef} className="form-control"/>
---
+++ src/model/AdminLogin.js
@@ -0,0 +1,12 @@
+const username="neoadmin@neosoftmail.com";
+const password="neoadmin321";
+
+
+export function checkLogin(adminemail, adminpass){
+    // connect to backend pass email, password
+    //get : email, password return object
+    if(adminemail==username && adminpass==password)
+        return true;
+    else
+        return false;
+}
