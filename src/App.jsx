cce820872a649ae9a598b5171158e43b1cd0b206
vinaneosoft <vinaneosoft@gmail.com>
2025-04-21 11:07:47 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,22 @@ import MyFooter from './components/MyFooter'
 import Learning from './components/Learning';
 import AdminLogin from './components/AdminLogin';
 import Employees from './components/Employees';
+import { useState } from 'react';
 
 /* functional component : UI+development */
  /* logic similar like html : jsx logic javascript extension */
  /* component : first letter captical 
  normal jsx elements : all letter small (similar like html) */
 function App() {
-  const mainheading="EMPLOYEE MANAGEMENT"; // data
+  const [mainheading, setHeading]= useState("DEVELOPER MANAGEMENT"); // data
   const companyName="Neosoft";
   const jsxelement=<h1 style={{textAlign:'center', color:'lightgreen'}}>{mainheading}</h1>
   
-  
+
+  setTimeout(() => {
+    //mainheading="Developer Management" // react wrong
+    setHeading("EMPLOYEE MANAGEMENT");
+  }, 3000);
   return (
    <>
     {jsxelement}
