f6395a4baf7eaa234d7e20d7d21cb5863adaaaf1
vinaneosoft <vinaneosoft@gmail.com>
2025-04-22 11:49:00 +0530

    comit

--- src/components/InputDemo.jsx
+++ src/components/InputDemo.jsx
@@ -1,8 +1,7 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function InputDemo(){
     let [empName, setName] =useState("");
-
     let empnameRef=useRef();
 
     function getEmployeeName(){
@@ -10,19 +9,42 @@ function InputDemo(){
        // empName=empnameRef.current.value;
        setName(empnameRef.current.value);
     }
+
+    let [experience, setExp]=useState(10);
+    useEffect(()=>console.log("UI rendered"), [empName, experience]);
     return (
-       <div>
-        <h5>Uncontrolled Input fields</h5>
-         <label>Enter Employee Name:</label>
-         <input type="text" defaultValue="sonali" ref={empnameRef} onBlur={getEmployeeName}></input>
-        <br></br> <small>in uncontrolled input filed you can not set initial value using
-            value attribute. 
-            use jsx attribute defaultValue in uncontrolled fields
-         </small>
-         <p>
-            Employee name is <b>{empName}</b>
-         </p>
-       </div>
+        <>
+            <div>
+                <h5>Uncontrolled Input fields</h5>
+                <label>Enter Employee Name:</label>
+                <input type="text" defaultValue="sonali" ref={empnameRef} onBlur={getEmployeeName}></input>
+                <br></br> <small>in uncontrolled input filed you can not set initial value using
+                    value attribute. 
+                    use jsx attribute defaultValue in uncontrolled fields
+                </small>
+                <p>
+                    Employee name is <b>{empName}</b>
+                </p>
+            </div>
+            <div>
+                <h5>Controlled Input fields</h5>
+                <label>Enter Experience:</label>
+                <input type="text" value={experience} onChange={(event)=>setExp(event.target.value)}></input>
+                <p>
+                    <small>controlled inputs fields are controlled by component by binding state variable
+                        in value attribute
+                        onChange is compulsory event handler to set the data to state variable
+                        otherwise field becomes readonly
+                    </small><br />
+                    <small>
+                        this  value, onChange, setter are compulsory steps in controlled input fields
+                    </small>
+                </p>
+                <p>
+                    Employee Experience is <b>{experience}</b>
+                </p>
+            </div>
+       </>
     );
 
 
