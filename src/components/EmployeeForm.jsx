2734d4784b607156d6d58e847c1dde7ed7d6f7ec
vinaneosoft <vinaneosoft@gmail.com>
2025-04-22 12:39:30 +0530

    commit

--- src/components/EmployeeForm.jsx
+++ src/components/EmployeeForm.jsx
@@ -1,40 +1,58 @@
 
 function EmployeeForm(){
+
+    const departments=[
+        {
+            deptId:'LD',
+            deptName:'Learning Devlopment'
+        },
+        {
+            deptId:'PT',
+            deptName:'Python'
+        },
+        {
+            deptId:'DN',
+            deptName:'Dot Net'
+        }
+    ]
+
     return(
         <>
          <h4 className="text-center">EMPLOYEE INPUT FORM</h4>
          <div className="d-flex justify-content-center p-3">
             <form className="bg-secondary w-50 p-3" >
                 <div className="mb-3">
-                    <label  className="form-label">ID</label>
-                    <input type="text"  className="form-control"/>
+                    <label className="form-label">NAME</label>
+                    <input type="text" className="form-control"/>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="" className="form-label">NAME</label>
-                    <input type="text"  className="form-control"/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="" className="form-label">PROFILE</label>
+                    <label className="form-label">PROFILE</label>
                     <input type="text"  className="form-control"/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="" className="form-label">SHORT DESCRIPTION</label>
-                    <input type="text"  className="form-control"/>
+                    <textarea  className="form-control"></textarea>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="" className="form-label">DEPARTMENT ID</label>
-                    <input type="text"  className="form-control"/>
+                    <select className="form-select">
+                        {
+                            departments.map((dept,idx)=><option key={idx} value={dept.deptId}>
+                            {dept.deptName}
+                            </option>)
+                        }
+                    </select>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="" className="form-label">EXPERIENCE</label>
-                    <input type="text"  className="form-control"/>
+                    <label  className="form-label">EXPERIENCE (in year)</label>
+                    <input type="number"  className="form-control"/>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="" className="form-label">JOINING DATE</label>
-                    <input type="text"  className="form-control"/>
+                    <label  className="form-label">JOINING DATE</label>
+                    <input type="date"  className="form-control"/>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="" className="form-label">PROFILE PIC LINK</label>
+                    <label  className="form-label">PROFILE PIC LINK</label>
                     <input type="text"  className="form-control"/>
                 </div>
                 <button type="submit" className="mx-3 btn btn-primary">ADD</button>
