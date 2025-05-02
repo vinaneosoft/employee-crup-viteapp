e4bc1c72d6801e2a62ee06291a63a45ea3693083
vinaneosoft <vinaneosoft@gmail.com>
2025-05-02 10:58:21 +0530

    store

---
+++ src/store.js
@@ -0,0 +1,11 @@
+
+import { configureStore } from "@reduxjs/toolkit";
+
+const store=configureStore({
+   reducer: {}// we will configure the reducer here once we create it
+});
+
+
+export default store;
+
+//1. store created
